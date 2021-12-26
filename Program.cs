3ae42e9bba93bb7cdc6a0f51cead99c3c73bc19c
szmolkal <szmolkal@gmail.com>
2021-12-26 20:07:10 +0100

    orszagok MySql adatbázis lekérdezések Console alkalmazásban     - Oracle MySql.Data Nuget     - MySql Datareader objektum használata     - Using(){} blokk használata

--- Program.cs
+++ Program.cs
@@ -11,6 +11,38 @@ namespace Orszagok_adatbazis_CSharp
     {
         static void Main(string[] args)
         {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost"; // 127.0.0.1
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "orszagok";
+            //sb.CharacterSet = "latin2_hungarian_ci";
+            MySqlConnection connection = new MySqlConnection(sb.ToString());
+            try
+            {
+                MySqlCommand sql = connection.CreateCommand();
+                sql.CommandText = "SELECT `orszag`, `nepesseg`, `terulet` FROM `orszagok`";
+                connection.Open();
+                using (MySqlDataReader dr = sql.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        string orszag = dr.GetString("orszag");
+                        int nepesseg = dr.GetInt32("nepesseg");
+                        double terulet = dr.GetDouble("terulet");
+                        Console.WriteLine($"{orszag}; {nepesseg}; {terulet.ToString("0.00")}");
+                    }
+                    dr.Close();
+                } //
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+            connection.Close();
+            Console.WriteLine("\nA program vége.");
+            Console.WriteLine(connection.ToString());
+            Console.ReadLine();
         }
     }
 }
